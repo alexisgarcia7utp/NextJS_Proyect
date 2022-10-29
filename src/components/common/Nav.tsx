d6ca9fd8f2450006b2ab106ba6365b7098e492c1
alexisgarcia7utp <84916527+alexisgarcia7utp@users.noreply.github.com>
2022-10-29 17:03:10 -0500

    Icono

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link'
 import React, { FC, useContext } from 'react';
 import { userContext, UserContextProps } from '../context/UserContext';
+import Image from 'next/image';
 
 export const Nav : FC = () => {
   const { userName, setUsername, userDetail } = useContext<UserContextProps>(userContext);
   return (
     <div>
       <nav className="w-full flex gap-5 p-6 items-center text-2xl text-white">
+        <Link href="/">
+          <Image
+            src="/images/s.png"
+            alt="/images/s.png"
+            width={170}
+            height={33}
+          />
+        </Link>
+        {/*/ --5px-- */}
         <Link href="/">
             Home
         </Link>
